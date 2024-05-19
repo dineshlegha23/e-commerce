a9b55b39da142a87c7528d5ce345ddb5a773584e
Dinesh <dineshlegha23@gmail.com>
2024-05-19 16:16:07 +0530

    fixed css issues in navbar

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 
 const CartButtons = () => {
   return (
-    <div className="flex gap-8 p-3">
+    <div className="flex gap-10 p-3">
       <div className="flex text-2xl items-center gap-2 relative">
         <p>Cart</p>
         <FaShoppingCart />
-        <p className="absolute -top-[10px] -right-3 text-white w-6 h-6 flex justify-center items-center text-sm rounded-full bg-red-700">
+        <p className="absolute -top-[8px] -right-3 text-white w-6 h-6 flex justify-center items-center text-sm rounded-full bg-red-700">
           6
         </p>
       </div>
