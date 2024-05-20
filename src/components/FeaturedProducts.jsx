0b6d5ebb8d363efc9d47bed12586b4a25363713d
Dinesh <dineshlegha23@gmail.com>
2024-05-20 17:10:04 +0530

    added redux toolkit

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Error from "./Error";
 import Loading from "./Loading";
 import Product from "./Product";
+import { useDispatch, useSelector } from "react-redux";
+import { addProducts } from "../store/productsSlice";
+import { products_url } from "../utils/constants";
 
 const FeaturedProducts = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products.allProducts);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(products_url);
+      const data = await response.json();
+      dispatch(addProducts(data));
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+
   return (
     <section className="h-screen bg-[#f1f5f8] px-[85px]">
       <div className="flex flex-col gap-10 justify-center items-center h-full">
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
-  return <h4>loading element</h4>
-}
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-transparent/10">
+      <div className="h-[100px] w-[100px] rounded-full bg-blue-400 flex justify-center items-center">
+        <div className="w-[90px] h-[90px] border-t-8 border-t-white rounded-full animate-spin"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
