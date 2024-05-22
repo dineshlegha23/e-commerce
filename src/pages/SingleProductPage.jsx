895eb4166e157d433afba8c027b79335778cf34e
Dinesh <dineshlegha23@gmail.com>
2024-05-22 18:36:44 +0530

    added product information

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import { single_product_url } from "../utils/constants";
 import { Loading, ProductImages, Stars } from "../components/";
 import { formatPrice } from "../utils/helpers";
+import { TiTick } from "react-icons/ti";
 
 const SingleProductPage = () => {
   const [loading, setLoading] = useState();
@@ -72,9 +73,42 @@ const SingleProductPage = () => {
               {product?.name}
             </h1>
             <Stars />
-            <p className="text-brown text-xl tracking-widest font-bold">
+            <p className="text-brown text-xl tracking-widest font-bold mt-1">
               ${formatPrice(product?.price)}
             </p>
+            <p className="w-[555px] text-black/80 text-wider text-base leading-8">
+              {product?.description}
+            </p>
+            <section className="w-[300px] flex flex-col gap-5 mt-3">
+              <div className="flex w-full">
+                <p className="w-full font-bold text-black/70">Available :</p>
+                <p className="text-left w-full text-black/80 -ml-11">
+                  {product?.stock > 0 ? "In Stock" : "Out of Stock"}
+                </p>
+              </div>
+              <div className="flex justify-between">
+                <p className="w-full font-bold text-black/70">SKU :</p>
+                <p className="left w-full text-black/80 -ml-11">
+                  {product?.id}
+                </p>
+              </div>
+              <div className="flex justify-between">
+                <p className="w-full font-bold text-black/70">Brand :</p>
+                <p className="text-left w-full text-black/80 -ml-11 capitalize">
+                  {product?.company}
+                </p>
+              </div>
+            </section>
+            <hr className="mt-3 text-black/20" />
+            <div className="flex justify-between w-[255px] mt-6">
+              <p className="font-bold text-black/70 w-full">Colors:</p>
+              <div className="flex gap-2 w-full text-left">
+                <div className="w-6 h-6 bg-red-400 rounded-full">
+                  <TiTick color="white" size={24} />
+                </div>
+                <div className="w-6 h-6 bg-red-400 rounded-full opacity-40"></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
