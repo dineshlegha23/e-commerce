a5f27978969f4c138f91cfff479e82c16aec38b1
Dinesh <dineshlegha23@gmail.com>
2024-05-29 17:23:00 +0530

    added toal cart items number in navbar

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const CartButtons = () => {
+  const { cartItems } = useSelector((store) => store.cart);
   return (
     <div className="flex gap-10 p-3">
       <Link
@@ -12,7 +14,7 @@ const CartButtons = () => {
         <p>Cart</p>
         <FaShoppingCart />
         <p className="absolute -top-[6px] -right-3 text-white w-6 h-6 flex justify-center items-center text-sm rounded-full bg-red-700">
-          6
+          {cartItems?.length}
         </p>
       </Link>
       <div className="flex text-2xl items-center gap-2">
