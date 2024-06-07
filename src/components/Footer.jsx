6d38b419d39999506a2dd4af9749ccb5e60273a2
Dinesh <dineshlegha23@gmail.com>
2024-06-07 16:37:54 +0530

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,14 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+
 const Footer = () => {
-  return <h4>footer</h4>
-}
+  return (
+    <footer className="px-[85px] bg-black/90 tracking-widest text-white text-center py-7">
+      <p>
+        &copy; 2024 <span className="text-brown">ComfySloth</span> All rights
+        reserved
+      </p>
+    </footer>
+  );
+};
 
-const Wrapper = styled.footer`
-  height: 5rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background: var(--clr-black);
-  text-align: center;
-  span {
-    color: var(--clr-primary-5);
-  }
-  h5 {
-    color: var(--clr-white);
-    margin: 0.1rem;
-
-    font-weight: 400;
-    text-transform: none;
-    line-height: 1.25;
-  }
-  @media (min-width: 776px) {
-    flex-direction: row;
-  }
-`
-
-export default Footer
+export default Footer;
