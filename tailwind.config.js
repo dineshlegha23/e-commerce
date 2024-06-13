8c9ca8c4f0353ecbeee50b24022aa2d92155a1fd
Dinesh <dineshlegha23@gmail.com>
2024-06-13 18:04:52 +0530

    added animation for opening sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,10 @@ export default {
           "50%": { transform: "scale(10)", opacity: 0.5 },
           "100%": { transform: "scale(40)", opacity: 0 },
         },
+        widthfull: {
+          "0%": { transform: "translate(-100vw)" },
+          "100%": { transform: "translate(0px)" },
+        },
         bounced: {
           "0%": { opacity: 0, height: 0 },
           "100%": { opacity: "100%", height: "100%" },
@@ -25,6 +29,7 @@ export default {
       },
       animation: {
         "ripple-animation": "ripple 1s ease 1 forwards",
+        "width-animation": "widthfull 0.3s ease",
       },
       colors: {
         gray: "#3E3D40",
