50edf6de2bf2ed2baef0cb4483e0ff34c103a60e
Dinesh <dineshlegha23@gmail.com>
2024-06-21 16:31:13 +0530

    changed text

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -6,7 +6,7 @@ const Checkout = () => {
     <section className="min-h-[calc(100vh-174px)]">
       <Links home={true} link={"checkout"} />
       <div className="flex flex-col items-center justify-center mt-20 gap-10">
-        <h1 className="text-5xl font-bold">Thank you for placing your order</h1>
+        <h1 className="text-5xl font-bold">We're working on it</h1>
         <Link
           to={"/"}
           className="py-2 px-2 bg-brown hover:bg-brown/90 text-white rounded-md"
